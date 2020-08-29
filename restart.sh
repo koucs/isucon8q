73cec03de51610a61c5fd2b2108d6b63101b31c0
koucs <kou.cshu@gmail.com>
2020-08-29 22:04:19 +0900

    update deploy.sh 2

--- restart.sh
+++ restart.sh
@@ -1,6 +1,9 @@
 # restart.sh
+echo "start deploying"
 
-sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/python && git fetch origin master && git reset --hard origin/master && make'
+sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/python && git fetch origin master && git reset --hard origin/master && sh ./setup.sh'
 sudo systemctl restart torb.python.service
 sudo -H bash -c "echo '' > /var/log/h2o/access.log"
 sudo systemctl restart h2o.service
+
+echo "end deploying"
