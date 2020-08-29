b88f8ea5584222c76b0ba9045c8a603b8f906c10
koucs <kou.cshu@gmail.com>
2020-08-30 00:38:02 +0900

    mysql

--- restart.sh
+++ restart.sh
@@ -12,4 +12,10 @@ sudo -H bash -c "echo '' > /var/log/h2o/access.log"
 sudo -H bash -c "cp /home/isucon/torb/config/h2o/h2o.conf /etc/h2o/h2o.conf"
 sudo systemctl restart h2o.service
 
+sudo -H bash -c "cp /home/isucon/torb/config/mysql1/server.cnf /etc/my.cnf.d/server.cnf"
+if sudo mysqladmin -uroot status then
+  sudo mysqladmin -uroot flush-logs
+fi
+sudo systemctl restart mariadb
+
 echo "end deploying"
