0ea1cec087c9d0a5184eb32624a291f4c4e7892c
koucs <kou.cshu@gmail.com>
2020-08-29 22:57:05 +0900

    template 3

--- restart.sh
+++ restart.sh
@@ -1,7 +1,7 @@
 # restart.sh
 echo "start deploying"
 
-sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/go && git fetch origin master && git reset --hard origin/master && make'
+# sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/go && git fetch origin master && git reset --hard origin/master && make'
 sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/python && git fetch origin master && git reset --hard origin/master && sh ./setup.sh'
 sudo systemctl restart torb.python.service
 
