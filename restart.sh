22612512d56497cebb39675dcd2f4f0ac1814a0c
koucs <kou.cshu@gmail.com>
2020-08-30 00:12:41 +0900

    config

--- restart.sh
+++ restart.sh
@@ -4,6 +4,7 @@ echo "start deploying"
 # sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/go && git fetch origin master && git reset --hard origin/master && make'
 sudo -H -u isucon bash -c 'source /home/isucon/.bash_profile && cd /home/isucon/torb/webapp/python && git fetch origin master && git reset --hard origin/master && sh ./setup.sh'
 sudo -H bash -c "cp /home/isucon/torb/config/torb.python.service /etc/systemd/system/torb.python.service"
+sudo systemctl daemon-reload
 sudo systemctl restart torb.python.service
 
 # Clean up /var/log/h2o/access.log
